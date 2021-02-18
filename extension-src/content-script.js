64dea18bda78c32e7730a524ed591aae2b5e1aac
jhamadhav <jhamadhav28@gmail.com>
2021-02-18 22:26:09 +0530

    space bar function added

--- extension-src/content-script.js
+++ extension-src/content-script.js
@@ -1,5 +1,62 @@
 console.log('hi I\'m google meet extension')
 
 document.body.addEventListener('click', () => {
-    console.log(document.activeElement)
+    removeFocus()
+    // show()
 })
+
+// Buttons
+let micBtn, chatBar, videoBtn;
+
+// window.onload = () => {
+//     if (isMicOff()) {
+//         micBtn.click()
+//     }
+// }
+
+document.addEventListener('keydown', (e) => {
+    micBtn = findMic()
+    if (e.key == ' ' && isMicOff() && !isChatOnFocus()) {
+        micBtn.click()
+    }
+});
+
+document.addEventListener('keyup', (e) => {
+    micBtn = findMic()
+    if (e.key == ' ' && !isMicOff() && !isChatOnFocus()) {
+        micBtn.click()
+    }
+});
+
+const removeFocus = () => {
+    let focusBtn = document.activeElement
+    // focusBtn.blur()
+}
+const show = () => {
+    console.log("mic : " + isMicOff());
+    console.log("cam : " + isCamOff());
+    console.log("chat : " + isChatOnFocus());
+}
+
+const isMicOff = () => {
+    removeFocus()
+    if (micBtn.dataset.isMuted == 'false') {
+        return false
+    }
+    return true;
+}
+const isCamOff = () => {
+    videoBtn = findCam()
+    if (videoBtn.dataset.isMuted == 'false') {
+        return false;
+    }
+    return true;
+}
+const isChatOnFocus = () => {
+    chatBar = findChatBar();
+    if (document.activeElement == chatBar) {
+        return true;
+    }
+    return false;
+}
+
--- extension-src/finds.js
+++ extension-src/finds.js
@@ -1,3 +1,47 @@
 const findMic = () => {
+    let res = null;
+    let btn = document.querySelectorAll("[data-tooltip]");
 
+    for (let i = 0; i < btn.length; i++) {
+        let element = btn[i];
+        let tt = element.dataset.tooltip
+        if (tt && (tt.toLowerCase().includes('ctrl + d') || tt.toLowerCase().includes('⌘ + d'))) {
+            res = element;
+            // console.log(res)
+        }
+    }
+    return res;
+}
+
+const findChatBar = () => {
+    let chatInput = document.querySelectorAll('textarea')[0];
+    return chatInput;
+}
+
+const findCam = () => {
+    let res = null;
+    let btn = document.querySelectorAll("[data-tooltip]");
+
+    for (let i = 0; i < btn.length; i++) {
+        let element = btn[i];
+        let tt = element.dataset.tooltip
+        if (tt && (tt.toLowerCase().includes('ctrl + e') || tt.toLowerCase().includes('⌘ + e'))) {
+            res = element;
+            // console.log(res)
+        }
+    }
+    return res;
+}
+
+const findClose = () => {
+    let res = null;
+    let btn = document.querySelectorAll('[aria-label]');
+    for (let i = 0; i < btn.length; i++) {
+        let element = btn[i];
+        let tt = element.dataset.tooltipId
+        if (tt == 'tt-c10') {
+            res = element;
+        }
+    }
+    return res;
 }
