4557e9f8648bde8faa3cd46464b256544d6cb76a
jhamadhav <jhamadhav28@gmail.com>
2021-02-23 09:32:36 +0530

    code cleaned

--- extension-src/content-script.js
+++ extension-src/content-script.js
@@ -1,40 +1,25 @@
 console.log('hi I\'m google meet extension')
 
-document.body.addEventListener('click', () => {
-    // removeFocus()
-})
-
-setInterval(() => {
-    let chatIcon = findChatIcon()
-    chatIcon.blur()
-}, 800)
-
 // Buttons
 let micBtn, chatBar, videoBtn;
-let key = 17
+let key = 18;
 const addKeyPressEvent = () => {
     document.addEventListener('keydown', (e) => {
-        // removeFocus()
+
         micBtn = findMic()
-        if (e.keyCode == key && isMicOff() && !isChatOnFocus()) {
+        if (e.keyCode == key && isMicOff()) {
             micBtn.click()
         }
     });
 
     document.addEventListener('keyup', (e) => {
-        // removeFocus()
         micBtn = findMic()
-        if (e.keyCode == key && !isMicOff() && !isChatOnFocus()) {
+        if (e.keyCode == key && !isMicOff()) {
             micBtn.click()
         }
     });
 }
-const removeFocus = () => {
-    let focusBtn = document.activeElement
-    if (!isChatOnFocus()) {
-        focusBtn.blur()
-    }
-}
+
 
 const isMicOff = () => {
     if (micBtn.dataset.isMuted == 'false') {
@@ -48,13 +33,6 @@ const isCamOff = () => {
         return false;
     }
     return true;
-}
-const isChatOnFocus = () => {
-    chatBar = findChatBar();
-    if (document.activeElement == chatBar) {
-        return true;
-    }
-    return false;
 }
 
 addKeyPressEvent()
